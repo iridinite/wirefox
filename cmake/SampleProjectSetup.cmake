cecbe4d3e58af54343d516cb5e9d488cd0061994
iridinite <codingcuddlewolf@gmail.com>
2019-04-18 16:49:07 +0200

    If at first you don't succeed, you fail

--- cmake/SampleProjectSetup.cmake
+++ cmake/SampleProjectSetup.cmake
@@ -1,5 +1,5 @@
 # Common logic for example projects
-add_executable(${LIBRARY_NAME})
+add_executable(${LIBRARY_NAME} "")
 target_link_libraries(${LIBRARY_NAME} PRIVATE Wirefox)
 include(${CMAKE_SOURCE_DIR}/cmake/CompilerSpecific.cmake)
 copy_wirefox_library()
